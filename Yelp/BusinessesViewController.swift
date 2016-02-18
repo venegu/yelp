ec75e5099c0fe968d87cc66443e66876160a92e4
venegu <venegu@gmail.com>
2016-02-18 17:19:04 -0500

    Adding search bar to navigation that does not currently work

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,25 +8,40 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     var businesses: [Business]!
+    var filteredBusinesses: [Business]!
+    
+    var searchBar : UISearchBar!
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Creating search bar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Enter a business name"
+        
+        // Adding search bar to the navigation as a navigation item
+        navigationItem.titleView = searchBar
+        
         tableView.dataSource = self
         tableView.delegate = self
         
         // Specifying table view row height based on auto layout constraints
         tableView.rowHeight = UITableViewAutomaticDimension
-        // To speed things up
+        
+        // To speed things up when opening the app - dynamic cell heights take time to calculate
         tableView.estimatedRowHeight = 120
 
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
+            
+            self.filteredBusinesses = self.businesses
             self.tableView.reloadData()
             
             for business in businesses {
@@ -36,9 +51,13 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         })
 
 /* Example of Yelp search with more search options specified
-        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+        Business.searchWithTerm("Restaurant", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             
+            if (self.businesses != nil) {
+                self.filteredBusinesses = self.businesses
+                self.tableView.reloadData()
+            }
             for business in businesses {
                 print(business.name!)
                 print(business.address!)
@@ -52,6 +71,10 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
     
+    
+    /**********
+     Table View
+    ***********/
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if businesses != nil {
             return businesses!.count
@@ -69,7 +92,33 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         return cell
         
     }
-
+    
+    /**********
+     Search Bar
+    ***********/
+    
+    // Searching for items related to search and displaying those
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredBusinesses = searchText.isEmpty ? businesses : businesses!.filter({ (business: Business) -> Bool in
+            return (business.name)!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        tableView.reloadData()
+    }
+    
+    // Displaying cancel button
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    // Returning intial data back to table view & resigning first responder when cancel is clicked
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredBusinesses = businesses
+        tableView.reloadData()
+    }
+    
     /*
     // MARK: - Navigation
 
