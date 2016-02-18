1c908c6706a04502337f029bdbf2f78687e56b30
venegu <venegu@gmail.com>
2016-02-18 13:14:50 -0500

    Adding basic table view with data

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,44 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Gale on 2/18/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbViewImage: UIImageView!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbViewImage.setImageWithURL(business.imageURL!)
+            categoriesLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!)"
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
