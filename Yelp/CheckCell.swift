1f8af965b5bab55ef82ccc3628dd486c688a388d
venegu <venegu@gmail.com>
2016-02-18 22:50:01 -0500

    Adding basic filter view

---
+++ Yelp/CheckCell.swift
@@ -0,0 +1,24 @@
+//
+//  CheckCell.swift
+//  Yelp
+//
+//  Created by Gale on 2/18/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class CheckCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
